7b823892f65297142c06bd7fc2c99a49af3ca177
구정현 <howhansume@gujeonghyeon-ui-Mac-mini.local>
2013-06-10 19:48:02 +0900

    Initial Commit

---
+++ ActozBanner/ActozBanner.h
@@ -0,0 +1,13 @@
+//
+//  ActozBanner.h
+//  ActozBanner
+//
+//  Created by 구정현 on 13. 6. 10..
+//  Copyright (c) 2013년 구정현. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActozBanner : NSObject
+
+@end
---
+++ ActozBanner/ActozBanner.m
@@ -0,0 +1,13 @@
+//
+//  ActozBanner.m
+//  ActozBanner
+//
+//  Created by 구정현 on 13. 6. 10..
+//  Copyright (c) 2013년 구정현. All rights reserved.
+//
+
+#import "ActozBanner.h"
+
+@implementation ActozBanner
+
+@end
